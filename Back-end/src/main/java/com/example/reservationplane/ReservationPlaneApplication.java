3e7cea91337ea257fafcf82602985fc1839d74ef
DESKTOP-C8761K8\Pablo <iwap18@gmail.com>
2022-07-30 13:03:50 +0200

    added rest of files backend

---
+++ Back-end/src/main/java/com/example/reservationplane/ReservationPlaneApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservationplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationPlaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationPlaneApplication.class, args);
+	}
+
+}
---
+++ Back-end/src/test/java/com/example/reservationplane/ReservationPlaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservationplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationPlaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
